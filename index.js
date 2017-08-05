7af9af387f9043ae0c9d6b8651955621ae36540c
Vladimir Borovik <bobahbdb@gmail.com>
2017-08-05 22:19:32 +0300

    Added slider

--- index.js
+++ index.js
@@ -14,14 +14,36 @@ const height = document.body.clientHeight;
 configCanvas(backCanvas, width, height);
 configCanvas(mainCanvas, width, height);
 
-const image2 = new Image();
-image2.src = './img/2.jpg';
+const images = [
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg'
+].map((src) => {
+  const image = new Image();
+  image.src = src;
+  return image;
+});
 
-const image1 = new Image();
-image1.src = './img/1.jpg';
-
-const image3 = new Image();
-image3.src = './img/3.jpg';
+const list = [
+  {
+    id: 0,
+    value: images[0],
+    next: 1,
+    prev: 2
+  },
+  {
+    id: 1,
+    value: images[1],
+    next: 2,
+    prev: 0
+  },
+  {
+    id: 2,
+    value: images[2],
+    next: 0,
+    prev: 1
+  },
+]
 
 const rowsCount = 50;
 const colsCount = 100;
@@ -43,12 +65,16 @@ const shufleGrid = () => {
 const dx = width / colsCount;
 const dy = height / rowsCount;
 
-const startK = 0;
-let k = startK;
-let into = false;
+const tl = new TimelineMax();
 
-document.addEventListener('click', () => {
-  into = !into;
+const k = {
+  pos: 0
+};
+
+document.addEventListener('click', (e) => {
+  const side = e.clientX < width / 2 ? 'left' : 'right';
+  if (side === 'right') forwardImage();
+  if (side === 'left') backwardImage();
 });
 
 const fromRange = (a, b, val) => {
@@ -58,21 +84,43 @@ const fromRange = (a, b, val) => {
 };
 
 shufleGrid();
-const currentImage = image1;
-const nextImage = image2;
+let prevImage = list[2];
+let currentImage = list[0];
+let nextImage = list[1];
+
+let currentImageIndex = 0;
+let nextImageIndex = 1;
+
+const forwardImage = () => {
+  tl.to(k, 1, {
+    pos: 2,
+    onComplete: () => {
+      currentImageIndex = nextImageIndex;
+      nextImageIndex = nextImageIndex + 1;
+      if (nextImageIndex === images.length) nextImageIndex = 0;
+      k.pos = 0;
+    }
+  });
+};
+
+const backwardImage = () => {
+  tl.to(k, 1, {
+    pos: 0,
+    onComplete: () => {
+      nextImageIndex = currentImageIndex;
+      currentImageIndex = currentImageIndex - 1;
+      if (currentImageIndex === -1) currentImageIndex = images.length - 1;
+      k.pos = 2;
+    }
+  });
+};
+
 const updateMainCanvas = () => {
-  if (k < 2 && into === true) {
-    k = fromRange(startK, 2, k + 0.04);
-  } else if (k > startK && into === false) {
-    k = fromRange(startK, 2, k - 0.04);
-  } else if (into === false) {
-    shufleGrid();
-  }
   mainCtx.clearRect(0, 0, width, height);
-  mainCtx.drawImage(currentImage, 0, 0, width, height);
+  mainCtx.drawImage(images[currentImageIndex], 0, 0, width, height);
   for (let i = 0; i < rowsCount; i += 1) {
     for (let j = 0; j < colsCount; j += 1) {
-      let delay = k - ((j) / (colsCount));
+      let delay = k.pos - ((j) / (colsCount));
       delay = fromRange(0, 1, delay);
       mainCtx.clearRect(j * dx, i * dy, dx * delay, dy * delay);
     }
@@ -81,7 +129,7 @@ const updateMainCanvas = () => {
 
 const render = (t) => {
   updateMainCanvas(t);
-  backCtx.drawImage(image2, 0, 0, width, height);
+  backCtx.drawImage(images[nextImageIndex], 0, 0, width, height);
   backCtx.drawImage(mainCanvas, 0, 0, width, height);
   requestAnimationFrame(render);
 };
